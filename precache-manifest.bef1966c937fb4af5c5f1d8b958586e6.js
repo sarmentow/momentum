51d97dfdb1a62abf4a482a5555a641ea8ed091e6
samentow <verschuer00@gmail.com>
2020-08-20 15:55:33 -0300

    Updates

---
+++ precache-manifest.bef1966c937fb4af5c5f1d8b958586e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf7866eb43d4e48c6ad93d78463a77b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "258bdc0b004b15f34627",
+    "url": "/static/css/main.cbfb3204.chunk.css"
+  },
+  {
+    "revision": "de260119d57ca5f2a3ca",
+    "url": "/static/js/2.5b7c37df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b7c37df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258bdc0b004b15f34627",
+    "url": "/static/js/main.30745d53.chunk.js"
+  },
+  {
+    "revision": "0f7474180d8585b301cd",
+    "url": "/static/js/runtime-main.b51ef498.js"
+  },
+  {
+    "revision": "21921f6a6e263032dc64e7aff7b11c45",
+    "url": "/static/media/moon.21921f6a.svg"
+  },
+  {
+    "revision": "0a7e2b2a36c634a4fd943a567239178e",
+    "url": "/static/media/sun.0a7e2b2a.svg"
+  }
+]);
